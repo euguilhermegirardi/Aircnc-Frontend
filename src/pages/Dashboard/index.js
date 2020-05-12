57fe0a102bd16938516c741c02effc2e9e47fe0f
Guilherme Girardi <girardi.gui@icloud.com>
2020-05-12 13:48:48 +0100

    advanced configurations done

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,16 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import api from '../../services/api';
 import './styles.css';
 import { Link } from 'react-router-dom';
+import socketio from "socket.io-client";
 
 export default function Dashboard() {
 
   // useState([]) = array cos' I'm expecting a list of stops and not just one.
   const [spots, setSpots] = useState([]);
+  const [requests, setRequests] = useState([]);
+
+  const user_id = localStorage.getItem('user');
+  // To memorize the user logged in.
+  const socket = useMemo(() => socketio('http://localhost:3333', {
+    query: { user_id },
+  }), [user_id]);
+
+  // useEffect fire the function as soon as the component show up in the screen.
+  // socket.io to connect the backend with the frontend.
+  useEffect(() => {
+    socket.on('booking_request', data => {
+      setRequests([...requests, data]);
+    })
+  }, [requests, socket]);
 
   // useEffect to load all the information that is already saved in the Dashboard.
   // useEffect is a function which has two params, the first one is a function, the second one is an 'array of dependencies'.
-  // The 'array of dependencies' has several variables and when these variables are uptaded the first function will be fired.
+  // The 'array of dependencies' has several variables and when these variables are updated the first function will be fired.
   // When we have an empty array, the first function will be fired just one time in this component.
   useEffect(() => {
     async function loadSpots() {
@@ -26,8 +42,33 @@ export default function Dashboard() {
     loadSpots();
   }, []);
 
+  async function handleAccept(id) {
+    await api.post(`/bookings/${id}/approvals`);
+    setRequests(requests.filter(request => request._id !== id));
+  };
+
+  async function handleDecline(id) {
+    await api.post(`/bookings/${id}/declines`);
+    setRequests(requests.filter(request => request._id !== id));
+  };
+
    return  (
     <>
+    <ul className="notifications">
+      {requests.map(request => (
+        <li key={request._id}>
+          <p>
+            <strong>{request.user.email}</strong> requested a spot in
+            <strong> {request.spot.company}</strong> on:
+            <strong> {request.date}</strong>
+          </p>
+          <button className="accept" onClick={() => handleAccept(request._id)}>Accept</button>
+          <button className="decline" onClick={() => handleDecline(request._id)}>Decline</button>
+        </li>
+        )
+      )}
+    </ul>
+
     <ul className="spot-list">
       {spots.map(spot => (
         <li key={spot._id}>
